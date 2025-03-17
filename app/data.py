23178ff8c7a826ba483429ec00d98096a9ee6768
Noah Perea <noahperea323@gmail.com>
2025-03-17 16:34:38 -0700

    undo data reformat

--- app/data.py
+++ app/data.py
@@ -5,8 +5,9 @@ from requests import get
 import streamlit as st
 import os
 
-API_KEY = st.secrets["API_KEY"]
-
+headers = {
+    "authorization": st.secrets["API_KEY"]
+}
 
 def get_data() -> dict:
 
@@ -20,19 +21,17 @@ def get_data() -> dict:
         else:
             with open("asteroids.json", "w") as file:
                 dump(raw_data, file, indent = 4, sort_keys= True)
-            
             neos_data = raw_data.get('near_earth_objects', {})
             #exctract the NEO's for the given date and the specified characteristics of each NEO
             neo_dict = {date_key: [{ "name": neo['name'],
-                                "diameter km":int(neo['estimated_diameter']['kilometers']['estimated_diameter_max']),
-                                "diameter ft":int(neo['estimated_diameter']['feet']['estimated_diameter_max']),
+                                "diameter km":neo['estimated_diameter']['kilometers']['estimated_diameter_max'],
+                                "diameter ft":neo['estimated_diameter']['feet']['estimated_diameter_max'],
                                 'hazardous': neo['is_potentially_hazardous_asteroid'],
-                                "velocity_Km": int(neo['close_approach_data'][0]['relative_velocity']['kilometers_per_hour']),
-                                "velocity_miles": int(neo['close_approach_data'][0]['relative_velocity']['miles_per_hour']),
-                                "miss_distance_km": int(neo['close_approach_data'][0]['miss_distance']['kilometers']),
-                                "miss_distance_miles": int(neo['close_approach_data'][0]['miss_distance']['miles'])
+                                "velocity_Km": neo['close_approach_data'][0]['relative_velocity']['kilometers_per_hour'],
+                                "velocity_miles": neo['close_approach_data'][0]['relative_velocity']['miles_per_hour'],
+                                "miss_distance_km": neo['close_approach_data'][0]['miss_distance']['kilometers'],
+                                "miss_distance_miles": neo['close_approach_data'][0]['miss_distance']['miles']
                                 } for neo in neos_on_date ] for date_key, neos_on_date in neos_data.items() }
             return neo_dict
     except Exception as e:
         print(e)
-
