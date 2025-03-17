6f559f0852116c51de28b85a6e22d0bb97a7f640
Noah Perea <noahperea323@gmail.com>
2025-03-17 15:55:09 -0700

    table updates

--- app/NEA_Tracker.py
+++ app/NEA_Tracker.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from data import get_data
 import altair as alt
+import pandas as pd
 st.set_page_config(
     page_title="Near Earth Asteroid Tracker"
 )
@@ -9,5 +10,6 @@ objects = get_data()
 
 st.title("Near earth Objects over the past week")
 st.write(objects)
-st.dataframe(objects,column_order=("col2","col1"))
+df = pd.DataFrame(objects, columns = (objects[i]['name'] for i in len(objects)))
+st.dataframe(df)
 
