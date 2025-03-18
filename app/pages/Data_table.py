0be2f6d9528a37c21d69b8120fe43a706bde01a9
Noah Perea <noahperea323@gmail.com>
2025-03-18 09:49:48 -0700

    Removed column format from radio slider, and changed it to horizontal, also added refresh button to the data table page

--- app/pages/Data_table.py
+++ app/pages/Data_table.py
@@ -1,12 +1,13 @@
 import streamlit as st
-from data import get_data
+from data import get_data,reset_cache
 import pandas as pd
 
 objects,total_count = get_data()
 
 st.title("Table view")
+refresh_button = st.sidebar.button("Refresh", type="primary", on_click=reset_cache)
 col1, col2 =st.columns([3,1])
-units = col2.st.radio("Select the Units: ",("Metric","Imperial"))
+units = st.radio("Select the Units: ",("Metric","Imperial"),horizontal=True)
 df = pd.DataFrame(objects)
 if units == "Metric":
     df = df.drop(columns=["Diameter (ft)", "Velocity (miles/h)"], errors="ignore")
