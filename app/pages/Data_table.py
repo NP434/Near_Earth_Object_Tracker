c3933feabbbb84028bb7fa56b71224398248b1bb
Noah Perea <noahperea323@gmail.com>
2025-03-18 09:52:16 -0700

    Added selector to a column

--- app/pages/Data_table.py
+++ app/pages/Data_table.py
@@ -7,7 +7,8 @@ objects,total_count = get_data()
 st.title("Table view")
 refresh_button = st.sidebar.button("Refresh", type="primary", on_click=reset_cache)
 col1, col2 =st.columns([3,1])
-units = st.radio("Select the Units: ",("Metric","Imperial"),horizontal=True)
+with col2:
+    units = st.radio("Select the Units: ",("Metric","Imperial"),horizontal=True)
 df = pd.DataFrame(objects)
 if units == "Metric":
     df = df.drop(columns=["Diameter (ft)", "Velocity (miles/h)"], errors="ignore")
