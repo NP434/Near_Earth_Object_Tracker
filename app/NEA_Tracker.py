228f6897b63cb934c36a673ce87039892b4f685e
Noah Perea <noahperea323@gmail.com>
2025-03-17 17:31:23 -0700

    Chart creation

--- app/NEA_Tracker.py
+++ app/NEA_Tracker.py
@@ -13,16 +13,17 @@ st.title("Near earth Objects")
 total_objects = 0
 counts = {}
 for date, date_data in objects.items():
-    if date != "total_objects":  # Ensure we don't count the total key
+    if date != "total_objects":
         daily_count = len(date_data)
         counts[date] = daily_count
         total_objects += daily_count
 
-    # Display counts in Streamlit
+
+
+st.write(f"### Total number of objects over past 7 days: {total_objects}")
 st.write("### NEO Count Per Day:")
-st.write(f"Total number of objects over past 7 days: {total_objects}")
 for date, count in counts.items():
     st.write(f"**{date}**: {count} NEO(s)")
-
-
+chart_data = pd.DataFrame(counts, columns= counts[date])
+st.bar_chart(chart_data)
 
