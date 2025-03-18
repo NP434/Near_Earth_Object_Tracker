ed0eaac48ce016ca9ac052890dd4494afa11598c
Noah Perea <noahperea323@gmail.com>
2025-03-18 09:36:05 -0700

    Attempted to add a units selection to the data table page

--- app/pages/Data_table.py
+++ app/pages/Data_table.py
@@ -3,7 +3,11 @@ from data import get_data
 import pandas as pd
 
 objects,total_count = get_data()
-
+units = st.radio("Select the Units: ",("Metric","Imperial"))
 st.title("Table view")
 df = pd.DataFrame(objects)
+if units == "Metric":
+    df = df.drop(columns=["Diameter (ft)", "Velocity (miles/h)"], error="ignore")
+else:
+    df = df.drop(columns=["Diameter (km)", "Velocity (km/h)"], error="ignore")
 st.dataframe(df, hide_index=True)
